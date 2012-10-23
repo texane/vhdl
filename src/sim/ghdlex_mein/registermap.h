e67e77690238dcf3139d39d3fbabf03c1de1c93e
Fabien Le Mentec <texane@gmail.com>
2012-10-23 16:13:13 -0500

    [ add ] sim/ghdlex_mein

---
+++ src/sim/ghdlex_mein/registermap.h
@@ -0,0 +1,5 @@
+#define FPGA_Registermap_Offset 0
+#define Reg_MagicId                                  (FPGA_Registermap_Offset + 0x00)
+#define Reg_Control                                  (FPGA_Registermap_Offset + 0x02)
+#define R_FPGA_Registermap_Control Reg_Control
+#define THROTTLE 0
